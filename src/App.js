af6c6e922c1a3e9e9b03864f21c589156b343207
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-06-05 18:10:04 +0700

    Up

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState(null); // State to hold the fetched data
+  const [error, setError] = useState(null); // State to hold any error that occurs during fetching
 
   useEffect(() => {
     fetch(
       "http://api3-env.eba-u35ryfqx.us-east-1.elasticbeanstalk.com/api/Loai"
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
       .then((data) => {
         setData(data); // Update the state with the fetched data
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        setError(error.message); // Update the error state
       });
   }, []); // Empty dependency array ensures useEffect only runs once after initial render
 
@@ -23,7 +30,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Data from API:</h1>
-        {data ? (
+        {error ? (
+          <p>Error: {error}</p>
+        ) : data ? (
           <ul>
             {data.map((item, index) => (
               <li key={index}>{item.tenLoai}</li>
